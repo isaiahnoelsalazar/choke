a7f4e267447dfab6a4059951070f2e89a65ea03e
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2026-03-01 20:03:52 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine(args[0]);
+        }
+        catch { }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# choke
+choke is a small interpreted programming language in C#, built as a practice project to explore language design and runtime concepts.
